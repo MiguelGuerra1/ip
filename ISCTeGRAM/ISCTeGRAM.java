9f0b5bf6ecfe1c4104b9b9bc825b3d0c65af23ab
Battousai <mauro.s.pinto@gmail.com>
2013-12-01 18:49:12 +0000

    historico

--- ISCTeGRAM/ISCTeGRAM.java
+++ ISCTeGRAM/ISCTeGRAM.java
@@ -3,61 +3,78 @@ import aguiaj.iscte.ColorImage;
 class ISCTeGRAM {
 
     protected final ColorImage _img;
+    protected final ImageHistory _history;
 
     ISCTeGRAM(ColorImage img)
     {
         _img = ImageUtil.copy(img);
+        _history = new ImageHistory(_img);
     }
     
     public void noise(int intensity)
     {
         FilterNoise.factory(intensity).apply(_img);
+        _history.add(_img);
     }
     
     public void contrast(int intensity)
     {
         FilterContrast.factory(intensity).apply(_img);
+        _history.add(_img);
     }
     
     public void vignette(int thresholdDistance)
     {
         FilterVignette.factory(thresholdDistance).apply(_img);
+        _history.add(_img);
     }
     
     public void sepia()
     {
         FilterSepia.factory().apply(_img);
+        _history.add(_img);
     }
     
     public void blur(int radius)
     {
         FilterBlur.factory(radius).apply(_img);
+        _history.add(_img);
     }
     
     public void film(ColorImage film, int factor)
     {
         FilterFilm.factory(film, factor).apply(_img);
+        _history.add(_img);
     }
     
     public void film(ColorImage film)
     {
         FilterFilm.factory(film).apply(_img);
+        _history.add(_img);
     }
     
     public void old()
     {
         CompositeEffect.OLD.apply(_img);
+        _history.add(_img);
     }
     
     public void retro()
     {
         CompositeEffect.RETRO.apply(_img);
+        _history.add(_img);
     }
     
-    /*
-    public int getLuminance(int x, int y)
+    public void undo()
     {
-        return _img.getColor(x, y).getLuminance();
-    }*/
+        if ( ! _history.firstPosition() && _history.canUndo())
+            ImageUtil.copy(_img, _history.undo());
+    }
+    
+    public void redo()
+    {
+        if (_history.canRedo())
+            ImageUtil.copy(_img, _history.redo());
+    }
 
 }
---
+++ ISCTeGRAM/ImageHistory.java
@@ -0,0 +1,76 @@
+import aguiaj.iscte.ColorImage;
+
+class ImageHistory {
+
+    public static final int DEFAULT_LENGTH = 10;
+
+    protected final ColorImage[] _history;
+    
+    protected int _pointer = -1;
+
+    ImageHistory(ColorImage img)
+    {
+        _history = new ColorImage[DEFAULT_LENGTH];
+        push(img);
+    }
+
+    ImageHistory(ColorImage img, int length)
+    {
+        _history = new ColorImage[length];
+        push(img);
+    }
+    
+    public void add(ColorImage img)
+    {
+        if (lastPosition())
+            return;
+
+        push(img);
+        
+        // remover resto do histórico para novos eventos
+        for (int i = _pointer + 1; i < _history.length; i++) {
+            if (_history[i] == null)
+                return;
+            
+            _history[i] = null;
+        }
+    }
+    
+    protected void push(ColorImage img)
+    {
+        _pointer++;
+        _history[_pointer] = ImageUtil.copy(img);
+    }
+    
+    public ColorImage undo()
+    {
+        _pointer--;
+        return _history[_pointer];
+    }
+    
+    public ColorImage redo()
+    {
+        _pointer++;
+        return _history[_pointer];
+    }
+    
+    public boolean canRedo()
+    {
+        return ! lastPosition() && _history[_pointer + 1] != null;
+    }
+    
+    public boolean canUndo()
+    {
+        return _pointer - 1 >= 0 && _history[_pointer - 1] != null;
+    }
+    
+    public boolean firstPosition()
+    {
+        return _pointer == 0;
+    }
+    
+    public boolean lastPosition()
+    {
+        return _pointer == _history.length - 1;
+    }
+}
